f6c05c9c95bf12dc414065be817f800466ee8c03
Shivesh Gupta <42131203+shivesh947@users.noreply.github.com>
2019-10-16 22:06:00 +0530

    files

---
+++ index.js
@@ -0,0 +1,11 @@
+function formopn()
+{
+    document.getElementById('frmdata').style.display="block";
+    document.getElementById('showinfo').innerHTML=" ";
+}
+function showinfo()
+{
+    document.getElementById('frmdata').style.display="none";
+    document.getElementById('showinfo').style.display="block";
+}
+
